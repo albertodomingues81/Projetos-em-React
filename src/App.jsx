d7c35d21f27c18ea446b51693546828bd97c0ea4
Alberto Domingues <94317971+albertodomingues81@users.noreply.github.com>
2022-06-10 18:33:57 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+
+const name = 'Alberto'
+const newName = name.toUpperCase()
+
+  function sum(a, b) {
+    return a + b 
+  }
+
+  const url = 'https://via.placeholder.com/150'
+  
+  return (
+    <div className= 'App'>
+    <h2>Alterando o JSX</h2>
+      <p>Óla, {newName}</p>
+      <p>Soma: {sum(1,2)}</p>
+      <img src={url} alt="Minha Imagem" />
+    </div>
+  );
+}
+
+export default App;
