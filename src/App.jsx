58159a9c9d15476167dcbc02758695ec7b1a34f8
Alberto Domingues <94317971+albertodomingues81@users.noreply.github.com>
2022-06-15 18:55:35 +0000

    Fragments

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './App.css';
-import HelloWorld from './components/HelloWorld'
 import SayMyName from './components/SayMyName'
 import Pessoa from './components/Pessoa'
-
+import Frase from './components/Frase'
+import List from './components/List'
 function App() {
 const nome = 'Maria' 
   
@@ -19,6 +19,7 @@ return (
         profissao="Programador"
         foto="https://via.placeholder.com/150"
         />
+       <List />
   </div>
   )      
 }
